85e85757359522d9be1b847aa9edd3f1d9237075
smaicon <smaicon@outlook.com>
2024-10-24 20:36:20 -0300

    adicionado arquivos `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 function Home() {
-
-    return <h1>Teste</h1>;
+  return <h1>Teste</h1>;
 }
-
 
 function Home2() {
-
-    return <h1>Java é melhor que Python</h1>;
+  return <h1>Java é melhor que Python</h1>;
 }
+
+function Home3() {
+  return
+}
+
 
 export default Home2;
